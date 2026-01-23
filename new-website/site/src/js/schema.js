8c3d43403464b98df281b3046ce388d6f0377c52
Ollie <m.oliver.smith@gmail.com>
2026-01-23 10:33:40 +0000

    content updates

---
+++ new-website/site/src/js/schema.js
@@ -0,0 +1,11 @@
+{
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "name": "Ollie Smith",
+    "url": "https://olliesmith.net",
+    "jobTitle": "Web Developer / Product Manager",
+    "description": "Web Developer and Product manager specialising in accessible, user-centred digital experiences for arts and cultural organisations.",
+    "sameAs": [
+        "https://www.linkedin.com/in/olliesmith-osos/"
+    ]
+}
